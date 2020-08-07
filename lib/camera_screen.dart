3f3cbff43deba2e440e0a34b34e106e62ea68819
Sam Moore <sam@betterment.com>
2020-08-07 13:47:51 -0400

    get some web rtc local stream rendering, create offer

---
+++ lib/camera_screen.dart
@@ -0,0 +1,46 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const CameraScreen({Key key, @required this.camera}) : super(key: key);
+
+  @override
+  CameraScreenState createState() => CameraScreenState();
+}
+
+class CameraScreenState extends State<CameraScreen> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(widget.camera, ResolutionPreset.medium);
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,77 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_webrtc/webrtc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final cameras = await availableCameras();
-  final firstCamera =
-      cameras.firstWhere((c) => c.lensDirection == CameraLensDirection.front);
+  final mediaConstraints = {
+    'audio': true,
+    'video': {
+      'mandatory': {
+        'minWidth': '640', // Provide your own width, height and frame rate here
+        'minHeight': '480',
+        'minFrameRate': '30',
+      },
+      'facingMode': 'user',
+      'optional': [],
+    }
+  };
+  final localStream = await navigator.getUserMedia(mediaConstraints);
+
+  final peerConfig = {
+    'iceServers': [
+      {'url': 'stun:stun.l.google.com:19302'},
+    ]
+  };
+  final peerConnection = await createPeerConnection(peerConfig, {});
+  peerConnection.addStream(localStream);
+  peerConnection.onIceCandidate = (_) {};
+  peerConnection.onIceConnectionState = (_) {};
+  peerConnection.onIceGatheringState = (_) {};
+
+  final offerConstraints = {
+    'mandatory': {
+      'OfferToReceiveAudio': true,
+      'OfferToReceiveVideo': true,
+    },
+    'optional': [],
+  };
+  final offer = await peerConnection.createOffer(offerConstraints);
+
+  peerConnection.setLocalDescription(offer);
+
+  // send offer to signaling server...
+  // _send('offer', {
+  //   'to': id,
+  //   'from': _selfId,
+  //   'description': {'sdp': s.sdp, 'type': s.type},
+  //   'session_id': this._sessionId,
+  //   'media': media,
+  // });
+
+  final localRenderer = RTCVideoRenderer();
+  await localRenderer.initialize();
+  localRenderer.srcObject = localStream;
 
   runApp(
     MaterialApp(
       theme: ThemeData.dark(),
-      home: CameraScreen(camera: firstCamera),
+      home: HomeScreen(localRenderer: localRenderer),
     ),
   );
 }
 
-class CameraScreen extends StatefulWidget {
-  final CameraDescription camera;
+class HomeScreen extends HookWidget {
+  final RTCVideoRenderer localRenderer;
 
-  const CameraScreen({Key key, @required this.camera}) : super(key: key);
-
-  @override
-  CameraScreenState createState() => CameraScreenState();
-}
-
-class CameraScreenState extends State<CameraScreen> {
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(widget.camera, ResolutionPreset.medium);
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  const HomeScreen({Key key, @required this.localRenderer}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Take a picture')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+      appBar: AppBar(title: Text('casual')),
+      body: RTCVideoView(localRenderer),
     );
   }
 }
