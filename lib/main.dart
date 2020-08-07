fc9100f40a68a3986b89e9887248514621eb6dc6
Sam Moore <sam@betterment.com>
2020-08-07 18:20:09 -0400

    working on making action cable work

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+
+import 'package:action_cable/action_cable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_webrtc/webrtc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await DotEnv().load('config/.env.local');
+  final env = DotEnv().env;
 
   final mediaConstraints = {
     'audio': true,
@@ -40,6 +46,59 @@ Future<void> main() async {
   final offer = await peerConnection.createOffer(offerConstraints);
 
   peerConnection.setLocalDescription(offer);
+
+  final authCredentials = base64Encode(
+    utf8.encode('${env["USERNAME"]}:${env["PASSWORD"]}'),
+  );
+
+  final cable = ActionCable.Connect(
+    'wss://cajzh.herokuapp.com/cable',
+    headers: {
+      'Authorization': 'Basic $authCredentials',
+      'Origin': 'https://cajzh.herokuapp.com',
+    },
+    onConnected: () {
+      print('connected');
+    },
+    onConnectionLost: () {
+      print('connection lost');
+    },
+    onCannotConnect: () {
+      print('cannot connect');
+    },
+  );
+
+  final roomId = env['ROOM_ID'];
+  cable.subscribe(
+    'Room',
+    channelParams: {'id': roomId},
+    onSubscribed: () => print('subscribed to room'),
+    onDisconnected: () => print('disconnected from room'),
+    onMessage: (message) {
+      switch (message['event_type']) {
+        case 'membership_created':
+          cable.performAction(
+            'Room',
+            action: 'create_offer',
+            channelParams: {'id': roomId},
+            actionParams: {
+              'to_membership_id': message['payload']['membership_id'],
+              'sdp': offer.sdp,
+            },
+          );
+          break;
+
+        case 'membership_destroyed':
+          break;
+
+        case 'offer_created':
+          break;
+
+        case 'answer_created':
+          break;
+      }
+    },
+  );
 
   // send offer to signaling server...
   // _send('offer', {
